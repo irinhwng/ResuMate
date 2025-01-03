8f45b621f946a63e92f618072bd0080a65defb95
Erin Hwang <erinhwang@Erins-MBP.lan>
2025-01-02 19:05:44 -0800

    rough draft of resume loader

--- app/controllers/resume_loader.py
+++ app/controllers/resume_loader.py
@@ -2,3 +2,29 @@
 This  file contains the applications API
 authors: Erin Hwang
 """
+import os
+from app.utils.logger import LoggerConfig
+from app.services.extractor import PDFExtractorChatGPT
+
+RESUME_PROMPT_NAME = os.getenv("RESUME_PROMPT_NAME")
+
+class ResumeLoader:
+    """
+    Loads resume content from a PDF file to extract relevant skills verbatim
+
+    Args:
+        file_path (str): Path to the PDF file to load
+
+    Returns:
+        str: The extracted resume content
+    """
+
+    def __init__(self, file_path: str):
+        self.logger = LoggerConfig().get_logger(__name__)
+        self.extractor = PDFExtractorChatGPT(prompt_name=RESUME_PROMPT_NAME, file_path=file_path)
+
+    @LoggerConfig().log_execution
+    def execute(self):
+        """Execute resume loading process"""
+        resume_str = self.extractor.lazy_load()
+        return resume_str
