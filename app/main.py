d0e8f95d6b8ace615aa32a1983cfc93be07155ed
Erin Hwang <erinhwang@Erins-MBP.lan>
2025-01-02 19:06:31 -0800

    add uuid feature when resume is uploaded

--- app/main.py
+++ app/main.py
@@ -2,12 +2,23 @@
 This file contains the application's API with Swagger documentation
 authors: Erin Hwang
 """
-
-from fastapi import FastAPI, Query, HTTPException
+import os
+from fastapi import FastAPI, Query, HTTPException, File, UploadFile
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
 from pydantic import HttpUrl
+import PyPDF2
+from pathlib import Path
+from app.utils.logger import LoggerConfig
+from uuid import uuid4
 
+# TODO: tmp storage --> use opensearch later on (close to production)
+pdf_storage = {} #key is uuiid, val is filepath
+
+logger = LoggerConfig().get_logger(__name__)
+
+UPLOADED_RESUME_PATH = os.getenv("UPLOADED_RESUME_PATH")
 # from app.services.scraper import JobScraperService
 from app.controllers.listing_loader import JobListingLoader
 
@@ -36,7 +47,7 @@ def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
-        title="Resumate API",
+        title="ResuMate API",
         version="1.0.0",
         description="API documentation for job listing analysis",
         routes=app.routes,
@@ -48,6 +59,56 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 @app.post(
+    "/upload-resume",
+    tags = ["resume"],
+    # response_model = str, #TODO
+    summary="Upload a resume PDF file",
+    description="Upload a PDF file containing resume content for generation"
+    )
+async def upload_resume(file: UploadFile):
+    """
+    Upload a resume PDF file for analysis
+
+    Args:
+        file (UploadFile): Resume PDF file
+
+    Returns:
+        str: Uploaded file name
+    """
+    if file.content_type != "application/pdf":
+        return JSONResponse(status_code=400, content={"message": "Invalid file type. Only PDF files are allowed."})
+
+    file_location = (Path(UPLOADED_RESUME_PATH) / file.filename).resolve()
+    #generate unique id for pdf
+    _file_uid = str(uuid4())
+
+    try:
+        # save the uploaded file to the resume directory
+        with open(str(file_location), "wb") as f:
+            f.write(await file.read())
+        logger.info(f"Uploaded resume file: {file.filename}")
+        pdf_storage[_file_uid] = str(file_location)
+        return JSONResponse(status_code=200, content = {
+            "message": "Resume uploaded successfully",
+            "resumate_uuid": _file_uid
+            }
+            ) #TODO: response model
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": f"An error occurred: {str(e)}"})
+
+@app.get(
+        "/current_resume_storage",
+        tags=["resume_storage"], #TODO: this is using tmp storage
+        summary="Get current resume storage",
+        description="Get the current resume storage for internal testing purposes"
+        )
+async def get_current_resume_storage():
+    """
+    Returns the current state of the PDF storage
+    """
+    return JSONResponse(status_code=200, content=pdf_storage)
+
+@app.post(
     "/scrape",
     tags=["scraper"],
     # response_model=ScrapeResponse, #TODO
@@ -55,6 +116,10 @@ app.openapi = custom_openapi
     description="Scrapes content from provided job listing URL"
 )
 async def scrape_url(
+    resumate_uuid: str = Query(
+        ...,
+        description = "ResuMate UUID of the uploaded resume (retrieved using /upload-resume)",
+    ),
     url: HttpUrl = Query(
         ...,
         description="URL of the job listing to scrape",
@@ -85,6 +150,15 @@ async def scrape_url(
     Returns:
         ScrapeResponse: Scraped content and status
     """
+
+    if resumate_uuid not in pdf_storage:
+        logger.error(f"ResuMate UUID not found in storage - please upload resume first")
+        return JSONResponse(
+            status_code=404,
+            content={
+                "message": "ResuMate UUID not found in storage - please upload resume first"
+                }
+                )
     try:
         job_data_loader = JobListingLoader(
             url,
